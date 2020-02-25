13506400509baf73c4044e78c3ccfba0a8f68e8e
Matthew Harshman <mjharshman66@gmail.com>
2020-02-24 17:02:36 -0800

    tried dif version, less effective

--- are_similiar.py
+++ are_similiar.py
@@ -43,3 +43,62 @@ def areSimilar(a, b):
             swap1 = k
             swap2 = a[i]
     return True
+
+
+
+    #############################################
+    #Dif version, less test cases passed
+    def areSimilar(a, b):
+    swapB = None
+    swapA = None
+
+    for i, b_val in enumerate(b):
+        
+        # same val
+        if a[i] == b_val:
+            if i + 1 == len(b):
+                if swapB:
+                    return False
+                else:
+                    return True
+            continue
+        
+        # mismatch happened
+        elif swapB:
+            
+            if swapA == b_val:
+                
+                # other pair
+                if swapB == a[i]:
+                  
+                  # end of list
+                  if i + 1 == len(b):
+                      return True
+                  
+                  # one remaining
+                  elif i + 2 == len(b):
+                    if b[-1] == a[-1]:
+                      return True
+                    else:  
+                      return False
+                
+                  else:
+                    arem = a[i+1:]
+                    brem = b[i+1:]
+                    print(arem,brem)
+                    for idx, val in enumerate(brem):
+                        if arem[idx] == val:
+                            continue
+                        else:
+                          return False
+                    return True
+                else:
+                  return False
+            else:
+              return False
+        elif i + 1 == len(b):
+            return False
+        else:
+            swapB = b_val
+            swapA = a[i]
+    return True
