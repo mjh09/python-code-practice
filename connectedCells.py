f670900cc7bca6c917d45517c36879511d23fc75
Matthew Harshman <mjharshman66@gmail.com>
2020-03-07 13:05:05 -0800

    upd funct, no good for runtime

--- connectedCells.py
+++ connectedCells.py
@@ -1,106 +1,104 @@
-matr = [[1,0,0,1,1],
-        [1,0,0,1,0],
-        [1,0,1,1,0],
-        [1,1,1,1,0]]
-# matr = [
-#         [1,0,1,1,0],
-#         [1,0,0,0,0],
-#         [1,0,1,1,0]
-# ]
-def con(matr):
-  region_dict = {}
+def connectedCell(matrix):
+  regions = {}
   last_segs = {}
-  cnt = 0
-
-  for end, row in enumerate(matr):
-    
-    #print(f'ROW---{row}')
-    one_segs = []
-    cur_seg = []
-    
+  cnt=0
+  for row in matrix:
+    one_idxs = [] # holds concurrent 1 indexs in loop (single list)
+    one_seg = [] # holds each segment of 1's from loops (list of lists)
     for idx, val in enumerate(row):
-    
       if val is 1:
-        cur_seg.append(idx)
-        if idx is len(row)-1:
-          #print(f'END IDX --{idx}')
-          one_segs.append(cur_seg)
+        one_idxs.append(idx)
+        if idx is len(row)-1: # end of row
+          one_seg.append(one_idxs)
         continue
-      
-      elif cur_seg:
-        one_segs.append(cur_seg)
-        cur_seg=[]
-        # cnt+=1
+      elif one_idxs:
+        one_seg.append(one_idxs)
+        one_idxs=[] # reset concurrent
       else:
         continue
+    
+    # after first regions
+    if regions:
+      #print(f'NEW ROW---{row} \n')
+      new_last_segs = {}
+      prev=None
+      for seg in one_seg:
 
-    if region_dict:
-      
-      curidxs = list(region_dict.values())
-      lastidxs = list(last_segs.values())
-      # print(f'CUR IDXS {curidxs}')
-      # print(f"ONE SEGS ---{one_segs}")
-      new_last_segs={}
-      for seg_idx in one_segs:
+        merged = 0
+        inc_seg = [i+1 for i in seg] # accounting for diagonals
+        dec_seg = [i-1 for i in seg]
         
-        f=0
-        
-        for i, idx in enumerate(seg_idx):
+        for key in list(last_segs.keys()):
+          arr = last_segs[key]
+          adj_set = set(arr) & set(seg)
+          diag1_set = set(arr) & set(inc_seg)
+          diag2_set = set(arr) & set(dec_seg)
           
-          t=0
-          m=None
-          
-          for key, val in last_segs.items():
-            
-            if idx in val:
-              #print(f'CUR IDXS {curidxs}')
-              #print(f"ONE SEGS ---{one_segs}")
-              #print(f"LAST IDXS --- {lastidxs}")
-              #print(f'CONNECTION FOUND --seg_idx={seg_idx}---idx={idx}---key, val={key,val}')
-              region_dict[key].extend(seg_idx)
-              new_last_segs[key] = seg_idx
-              f=1
-              t=1
-              m=key
-              break
-          
-          if t is 1:
-            #print(f'LAST SEGS--{last_segs}')
-            #print(f"UPDATED DICT {region_dict}")
-            remaining = seg_idx[i+1:]
-            #print(f'REMAINING --- {remaining}')
-            for r in remaining:
-              for rkey, rval in last_segs.items():
-                #print(f'K V{rkey,rval}---R{r}')
-                if r in rval:
-                  #print(f'MERGE SEGS')
-                  region_dict[cnt] = region_dict[m] + region_dict[rkey]
-                  break
+          if merged:
+            #print("PREV MERGE")
+            if adj_set:
+              regions[prev].extend(regions[key])
+              del regions[key]
+            elif diag1_set:
+              regions[prev].extend(regions[key])
+              del regions[key]
+            elif diag2_set:
+              regions[prev].extend(regions[key])
+              del regions[key]
+            else:
+              regions[cnt] = seg
+              cnt+=1
 
-            #print("BREAK HERE \n")
-            break
-        
-        # no connections found, create new region
-        if f is 0:
-          #print("no connections found, create new region")
-          region_dict[cnt] = seg_idx
-          new_last_segs[cnt] = seg_idx
-          cnt+=1
+          elif adj_set:
+            #print(f'ADJ SET')
+            regions[key].extend(seg)
+            if key is prev:
+              new_last_segs[key].extend(seg)
+              #print(f"MERGED--{regions} \n")
+            else:  
+              new_last_segs[key] = seg
+              merged, prev = 1 , key
+              #print(f"MERGED--{regions} \n")
+          elif diag1_set:
+            #print("DIAG1 SET")
+            regions[key].extend(seg)
+            if key is prev:
+              new_last_segs[key].extend(seg)
+              #print(f"MERGED--{regions} \n")
+            else:  
+              new_last_segs[key] = seg
+              merged, prev = 1 , key
+              #print(f"MERGED--{regions} \n")
+          elif diag2_set:
+            #print("DIAG2 SET")
+            regions[key].extend(seg)
+            if key is prev:
+              new_last_segs[key].extend(seg)
+              #print(f"MERGED--{regions} \n")
+            else:  
+              new_last_segs[key] = seg
+              merged, prev = 1 , key
+              #print(f"MERGED--{regions} \n")
+          else:
+            regions[cnt] = seg
+            new_last_segs[cnt] = seg
+            cnt+=1
       
-      last_segs = new_last_segs
-      #print(f'END NEW LAST SEGS -- {new_last_segs} \n')
-      if end is len(matr)-1:
-        final = list(region_dict.values())
-        fin=0
-        for arr in final:
-          length = len(arr)
-          if length > fin:
-            fin = length 
-        return fin     
+      last_segs=new_last_segs
+      #print(f"UPDATED LAST SEGS{last_segs} \n")
+    
+    # first regions
     else:
-      #print(f'one_segs--{one_segs}')
-      
-      for idx, val in enumerate(one_segs):
-        region_dict[idx] = val
-        last_segs[idx] = val
-        cnt +=1
+      for idx, arr in enumerate(one_seg):
+        regions[idx] = arr
+        last_segs[idx] = arr
+        cnt+=1
+      #print(f'FIRST REGIONS---{regions} \n')
+  
+  max_len = 0
+
+  for reg in list(regions.values()):
+    if len(reg) > max_len:
+      max_len = len(reg)
+  
+  return max_len
