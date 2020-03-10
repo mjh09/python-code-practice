ffce5a5a98331c06758f18a720d9152ad31152e4
Matthew Harshman <mjharshman66@gmail.com>
2020-03-10 10:01:35 -0700

    more sql

--- sql_practice.py
+++ sql_practice.py
@@ -12,3 +12,23 @@ SELECT Max(Salary)
 FROM Employees
 WHERE Salary <
     (SELECT Max(Salary) FROM Employees)
+'''
+
+'''
+SELECT DISTINCT TOP 2 Salary
+FROM Employees
+ORDER BY Salary DESC
+'''
+ # Top nth - change '2' to be nth
+'''
+SELECT TOP 1 Salary
+FROM
+    (SELECT DISTINCT TOP 2 Salary  
+    FROM Employees
+    ORDER BY Salary DESC) Result
+ORDER BY Salary
+'''
+ # applying rank
+'''
+Select Salary, DENSE_RANK () over (ORDER BY Salary DESC) as DENSERANK
+FROM Employees
