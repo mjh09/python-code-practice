c173c669d95e46d06114f129622b913db9be8983
Matthew Harshman <mjharshman66@gmail.com>
2020-03-11 17:22:32 -0700

    IQR for even arrays

--- interquartile_range.py
+++ interquartile_range.py
@@ -1,6 +1,6 @@
 import math
 ### steps
-numbers = [1,2,3,4,5,6,7,8,9]
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
 
 # find median -> Q2
 if len(numbers) % 2:
@@ -29,10 +29,26 @@ if len(numbers) % 2:
         print(f'IQR = {IQR}')
 else:
     half = int(len(numbers)/2)
-    half1 = half - 1
-    first = numbers[half1]
-    second = numbers[half]
+    first = numbers[:half]
+    second = numbers[half:]
+    print(first, second)
 
+    if len(first) % 2:
+        med = len(first) // 2
+        Qone = first[med]
+        Qthree = second[med]
+        print(f'Q1 = {Qone}, Q3 = {Qthree}')
+        IQR = Qthree - Qone
+        print(f'IQR = {IQR}')
+
+    else:
+        half = int(len(first) / 2)
+        half1 = half - 1
+        Qone = (first[half] + first[half1]) / 2
+        Qthree = (second[half] + second[half1]) / 2
+        print(f'Q1 = {Qone}, Q3 = {Qthree}')
+        IQR = Qthree - Qone
+        print(f'IQR = {IQR}')
 # find median of start to Q2 -> Q1
 # find median of Q2 to end -> Q3
 # difference between Q3 and Q1
